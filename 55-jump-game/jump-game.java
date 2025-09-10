0280f5c879370108c19f97ec5996acf8ca05ab6c
singhlalima <153114735+singhlalima@users.noreply.github.com>
2025-09-11 01:11:04 +0530

    Time: 2 ms (83.82%) | Memory: 45.8 MB (43.82%) - LeetSync

---
+++ 55-jump-game/jump-game.java
@@ -0,0 +1,52 @@
+class Solution {
+    public boolean canJump(int[] nums) {
+        int jump = nums[0];
+        int idx = nums.length-1;
+        int temp = 0;
+        int i = 0;
+        
+        while(i<nums.length){
+           
+            if((jump - (idx-i))>=0){
+                return true;
+            }
+            else{
+                temp = jump;
+                int flag  = 0;
+                while(temp>0){
+                    i++;
+                    if(nums[i]>=jump){
+                        jump = nums[i];
+                        flag=1;
+                        break;
+
+                    }
+                    else{
+                        if(nums[i]-(nums.length-1-i)>=0){
+                            flag =2;
+                            break;
+                        }
+                    }
+                    temp --;
+                    
+                }
+        
+                if (flag == 0){
+                    jump = nums[i];
+                }
+                
+                if((jump == 0) || (flag==2)){
+                    break;
+                }
+            }
+        }
+        if (jump == 0){
+            return false;
+        }
+        else{
+            return true;
+        }
+        
+        
+    }
+}

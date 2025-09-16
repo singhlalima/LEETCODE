d9808214f129d051da03a2fb20ce024c425e5866
singhlalima <153114735+singhlalima@users.noreply.github.com>
2025-09-16 15:38:06 +0530

    Time: 3 ms (98.31%) | Memory: 44.6 MB (48.04%) - LeetSync

--- 12-integer-to-roman/integer-to-roman.java
+++ 12-integer-to-roman/integer-to-roman.java
@@ -1,73 +1,92 @@
 class Solution {
     public String intToRoman(int num) {
-       int a = num/1000;
-       num = num%1000;
-       int b = num/100;
-       num = num%100;
-       int c = num/10;
-       num = num%10;
-       int d = num/1;
-       num = num%1;
-       String ans = Thousand(a)+Hundred(b)+Tens(c)+Ones(d);
-       return ans;
-        
-    }
-    public static String Ones(int x){
-        switch(x){
-            case 0: return "";
-            case 1: return "I";
-            case 2: return "II";
-            case 3: return "III";
-            case 4: return "IV";
-            case 5: return "V";
-            case 6: return "VI";
-            case 7: return "VII";
-            case 8: return "VIII";
-            case 9: return "IX";
-            default: return "";
-        }
-    }
+        final int[] values = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+        final String[] symbols = {"M", "CM", "D",  "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
 
-    public static String Tens(int x){
-        switch(x){
-            case 0: return "";
-            case 1: return "X";
-            case 2: return "XX";
-            case 3: return "XXX";
-            case 4: return "XL";
-            case 5: return "L";
-            case 6: return "LX";
-            case 7: return "LXX";
-            case 8: return "LXXX";
-            case 9: return "XC";
-            default: return "";
-        }
-    }
+        StringBuilder sb = new StringBuilder();
 
-    public static String Hundred(int x){
-        switch(x){
-            case 0: return "";
-            case 1: return "C";
-            case 2: return "CC";
-            case 3: return "CCC";
-            case 4: return "CD";
-            case 5: return "D";
-            case 6: return "DC";
-            case 7: return "DCC";
-            case 8: return "DCCC";
-            case 9: return "CM";
-            default: return "";
+        for (int i = 0; i < values.length; ++i) {
+            if (num == 0)
+                break;
+            while (num >= values[i]) {
+                sb.append(symbols[i]);
+                num -= values[i];
+            }
         }
-    }
 
-    public static String Thousand(int x){
-        switch(x){
-            case 0: return "";
-            case 1: return "M";
-            case 2: return "MM";
-            case 3: return "MMM";
-            
-            default: return "";
-        }
+        return sb.toString();        
     }
 }
+// class Solution {
+//     public String intToRoman(int num) {
+//        int a = num/1000;
+//        num = num%1000;
+//        int b = num/100;
+//        num = num%100;
+//        int c = num/10;
+//        num = num%10;
+//        int d = num/1;
+//        num = num%1;
+//        String ans = Thousand(a)+Hundred(b)+Tens(c)+Ones(d);
+//        return ans;
+        
+//     }
+//     public static String Ones(int x){
+//         switch(x){
+//             case 0: return "";
+//             case 1: return "I";
+//             case 2: return "II";
+//             case 3: return "III";
+//             case 4: return "IV";
+//             case 5: return "V";
+//             case 6: return "VI";
+//             case 7: return "VII";
+//             case 8: return "VIII";
+//             case 9: return "IX";
+//             default: return "";
+//         }
+//     }
+
+//     public static String Tens(int x){
+//         switch(x){
+//             case 0: return "";
+//             case 1: return "X";
+//             case 2: return "XX";
+//             case 3: return "XXX";
+//             case 4: return "XL";
+//             case 5: return "L";
+//             case 6: return "LX";
+//             case 7: return "LXX";
+//             case 8: return "LXXX";
+//             case 9: return "XC";
+//             default: return "";
+//         }
+//     }
+
+//     public static String Hundred(int x){
+//         switch(x){
+//             case 0: return "";
+//             case 1: return "C";
+//             case 2: return "CC";
+//             case 3: return "CCC";
+//             case 4: return "CD";
+//             case 5: return "D";
+//             case 6: return "DC";
+//             case 7: return "DCC";
+//             case 8: return "DCCC";
+//             case 9: return "CM";
+//             default: return "";
+//         }
+//     }
+
+//     public static String Thousand(int x){
+//         switch(x){
+//             case 0: return "";
+//             case 1: return "M";
+//             case 2: return "MM";
+//             case 3: return "MMM";
+            
+//             default: return "";
+//         }
+//     }
+// }

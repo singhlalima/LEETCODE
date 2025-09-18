539e53a0ea4bd3925022dcce786f6affac407182
singhlalima <153114735+singhlalima@users.noreply.github.com>
2025-09-18 14:58:00 +0530

    Time: 3 ms (90.69%) | Memory: 45.1 MB (58.94%) - LeetSync

--- 6-zigzag-conversion/zigzag-conversion.java
+++ 6-zigzag-conversion/zigzag-conversion.java
@@ -1,31 +1,31 @@
 class Solution {
     public String convert(String s, int numRows) {
         int n = s.length();
-        String ans = "";
+        StringBuilder ans = new StringBuilder("");
         if (n<=numRows) return s;
         if(numRows == 1) return s;
         for(int i = 0; i<numRows; i++){
            
             int idx = i;
-            ans += s.charAt(i);
+            ans.append(s.charAt(i));
             int add = ((numRows-i)*2)-2;
             int var = i*2;
             if(i==numRows-1) add = (numRows*2)-2;
             
             while(idx<n){
                 idx += add;
-                if(idx<n)ans += s.charAt(idx);
+                if(idx<n) ans.append(s.charAt(idx));
                 
                 if(i!=0 && i!=numRows-1){
                 idx += var;
                 if(idx<n)
-                ans += s.charAt(idx);}
+                ans.append(s.charAt(idx));}
             }
                 
                 
         }
             
-    return ans;
+    return ans.toString();
         
     }
     }

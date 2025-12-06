d9078ea1743e2cb927c17ed71c93bf8dd2467194
singhlalima <153114735+singhlalima@users.noreply.github.com>
2025-12-06 19:23:48 +0530

    Time: 7 ms (68.85%) | Memory: 49.2 MB (56.57%) - LeetSync

--- 49-group-anagrams/group-anagrams.java
+++ 49-group-anagrams/group-anagrams.java
@@ -1,43 +1,43 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-
-        HashMap<String, List<String>> map = new HashMap<>();
-
-        for (String s : strs) {
-
-            int[] freq = new int[26]; // for a to z
-
-            for (char c : s.toCharArray()) {
-                freq[c - 'a']++;
-            }
-
-            // Create a unique key from frequency
-            StringBuilder keyBuilder = new StringBuilder();
-            for (int count : freq) {
-                keyBuilder.append(count).append('#');
-            }
-            String key = keyBuilder.toString();
-
-            map.putIfAbsent(key, new ArrayList<>());
+        HashMap<String,List<String>> map = new HashMap<>();
+        for(String s: strs){
+            char[] arr = s.toCharArray();
+            Arrays.sort(arr);
+            String key = new String(arr);
+            map.putIfAbsent(key,new ArrayList<>());
             map.get(key).add(s);
         }
 
         return new ArrayList<>(map.values());
     }
 }
-
-
 // class Solution {
 //     public List<List<String>> groupAnagrams(String[] strs) {
-//         HashMap<String,List<String>> map = new HashMap<>();
-//         for(String s: strs){
-//             char[] arr = s.toCharArray();
-//             Arrays.sort(arr);
-//             String key = new String(arr);
-//             map.putIfAbsent(key,new ArrayList<>());
+
+//         HashMap<String, List<String>> map = new HashMap<>();
+
+//         for (String s : strs) {
+
+//             int[] freq = new int[26]; // for a to z
+
+//             for (char c : s.toCharArray()) {
+//                 freq[c - 'a']++;
+//             }
+
+//             // Create a unique key from frequency
+//             StringBuilder keyBuilder = new StringBuilder();
+//             for (int count : freq) {
+//                 keyBuilder.append(count).append('#');
+//             }
+//             String key = keyBuilder.toString();
+
+//             map.putIfAbsent(key, new ArrayList<>());
 //             map.get(key).add(s);
 //         }
 
 //         return new ArrayList<>(map.values());
 //     }
 // }
+
+

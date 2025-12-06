6ee31e61070e5ea19b9432cb17ff8176b0b93eeb
singhlalima <153114735+singhlalima@users.noreply.github.com>
2025-12-07 03:25:14 +0530

    Time: 39 ms (10.11%) | Memory: 88.8 MB (29.50%) - LeetSync

--- 219-contains-duplicate-ii/contains-duplicate-ii.java
+++ 219-contains-duplicate-ii/contains-duplicate-ii.java
@@ -1,35 +1,35 @@
 class Solution {
     public boolean containsNearbyDuplicate(int[] nums, int k) {
-        // HashMap<Integer,Integer> freq = new HashMap<>();
-        // HashMap<Integer,Integer> lastIndex = new HashMap<>();
-        // for(int i = 0;i<nums.length;i++){
-        //     freq.put(nums[i], freq.getOrDefault(nums[i],0)+1);
-            
+        HashMap<Integer,Integer> freq = new HashMap<>();
+        HashMap<Integer,Integer> lastIndex = new HashMap<>();
+        for(int i = 0;i<nums.length;i++){
+            freq.put(nums[i], freq.getOrDefault(nums[i],0)+1);
+
+        }
+
+        for(int i= 0; i<nums.length;i++){
+            if(freq.get(nums[i])>1){
+                if(lastIndex.containsKey(nums[i])){
+                    int prev = lastIndex.get(nums[i]);
+                    int curr = i;
+
+                    if(Math.abs(prev-curr)<=k) return true;
+                }
+                lastIndex.put(nums[i],i);
+            }
+        }
+        return false ;
+
+        // HashMap<Integer, Integer> map = new HashMap<>();
+
+        // for (int i = 0; i < nums.length; i++) {
+        //     if (map.containsKey(nums[i]) && i - map.get(nums[i]) <= k)
+        //         return true;
+
+        //     map.put(nums[i], i); // store/update last seen index
         // }
-        
-        // for(int i= 0; i<nums.length;i++){
-        //     if(freq.get(nums[i])>1){
-        //         if(lastIndex.containsKey(nums[i])){
-        //             int prev = lastIndex.get(nums[i]);
-        //             int curr = i;
 
-        //             if(Math.abs(prev-curr)<=k) return true;
-        //         }
-        //         lastIndex.put(nums[i],i);
-        //     }
-        // }
-        // return false ;
-
-        HashMap<Integer, Integer> map = new HashMap<>();
-
-for (int i = 0; i < nums.length; i++) {
-    if (map.containsKey(nums[i]) && i - map.get(nums[i]) <= k)
-        return true;
-
-    map.put(nums[i], i); // store/update last seen index
-}
-
-return false;
+        // return false;
 
     }
 }
